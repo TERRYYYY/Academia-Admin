9fa35f0af0c1e9e46071ace96699cae496fc7b37
TERRYYYY <kariuki.terry11@gmail.com>
2024-11-22 14:07:43 +0300

    Added some styling to chart 1

--- chart1.js
+++ chart1.js
@@ -3,18 +3,20 @@ const ctx = document.getElementById('lineChart');
   new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
       datasets: [{
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
+        data: [2050, 1900, 1800, 2800, 2000, 2500, 2600, 2450, 1950, 2300, 2900, 2500],
+        borderWidth: 1,
+        backgroundColor: [
+          'rgba(85, 85, 85, 1)',
+        ],
+        borderColor: [
+          'rgba(41, 155, 99)',
+        ]
       }]
     },
     options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+      responsive: true
     }
   });
